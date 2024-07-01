a6106f27e38df98b05d344dfa0dccd869ed09f5a
Marwane Skaro <marwanebenhadj@gmail.com>
2024-07-01 23:46:57 +0100

    Update Selenium_automation.py

--- Selenium_automation.py
+++ Selenium_automation.py
@@ -1,1 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+browser = webdriver.Firefox()
+browser.get('https://orteil.dashnet.org/cookieclicker/')
+
+
+time.sleep(5)
+
+
+toggle_box = browser.find_element(By.XPATH, '//*[@id="toggleBox"]')
+toggle_box.click()
+
+cookie = browser.find_element(By.ID, 'bigCookie')
+cookie.click()
+
+
+browser.quit()
 
